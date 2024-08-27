809def394ca5db8a167f15dd95ac94220dfce6b9
FelixAGonzales <FelixAGonzales93@gmail.com>
2024-08-26 22:07:57 -0400

    add

---
+++ store_item.rb
@@ -0,0 +1,7 @@
+consoles = [
+  { name: "Switch", price: 250, color: "blue" },
+  { :name => "Xbox", :price => 300, :color => "green" },
+  { name: "PS4", price: 350, color: "white" },
+]
+
+pp consoles
